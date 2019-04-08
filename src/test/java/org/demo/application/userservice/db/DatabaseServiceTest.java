93fcbf11ab4f7976c271670965c049b4ca577b00
Piotr <badzio92@gmail.com>
2019-04-08 22:41:23 +0200

    Change logging level to DEBUG Added two test cases

--- src/test/java/org/demo/application/userservice/db/DatabaseServiceTest.java
+++ src/test/java/org/demo/application/userservice/db/DatabaseServiceTest.java
@@ -5,6 +5,7 @@ import org.demo.application.userservice.db.model.UserEntity;
 import org.demo.application.userservice.db.repository.UserRepository;
 import org.demo.application.userservice.exception.NotExistUserException;
 import org.demo.application.userservice.model.User;
+import org.demo.application.userservice.util.DataBaseEntityBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -19,6 +20,8 @@ import static org.demo.application.userservice.util.DataBaseEntityBuilder.buildS
 import static org.demo.application.userservice.util.ModelBuilder.buildAddress;
 import static org.demo.application.userservice.util.ModelBuilder.buildUser;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,7 +34,42 @@ public class DatabaseServiceTest {
     private EntityMapper entityMapper;
 
     @InjectMocks
-    private DatabaseService userDbService;
+    private DatabaseService databaseService;
+
+    @Test
+    public void whenUserIsPassedToSave_UserShouldBeConvertedAndSave_IdShouldBeReturned(){
+
+        //given
+        Long id = 1L;
+        User user = buildUser(buildAddress());
+        UserEntity userEntity = DataBaseEntityBuilder.buildSingleUserEntity(buildSingleAddressEntity());
+
+        when(entityMapper.convertTo(eq(user))).thenReturn(userEntity);
+        when(userRepository.addUser(eq(userEntity))).thenReturn(id);
+
+        //when
+        Long userId = databaseService.save(user);
+
+        //then
+        assertThat(userId).isEqualTo(id);
+    }
+
+    @Test
+    public void whenUserIsPassedToUpdate_UserShouldBeConvertedAndPassedToUpdate(){
+
+        //given
+        Long id = 1L;
+        User user = buildUser(buildAddress());
+        UserEntity userEntity = DataBaseEntityBuilder.buildSingleUserEntity(buildSingleAddressEntity());
+
+        when(entityMapper.convertTo(eq(user))).thenReturn(userEntity);
+
+        //when
+        databaseService.update(id,user);
+
+        //then
+        verify(userRepository,times(1)).updateUser(eq(id),eq(userEntity));
+    }
 
     @Test
     public void whenUserIsPresentInDB_UserShouldBeReturned(){
@@ -45,7 +83,7 @@ public class DatabaseServiceTest {
         when(entityMapper.convertFrom(eq(userEntity))).thenReturn(user);
 
         //when
-        User result = userDbService.getUser(id);
+        User result = databaseService.getUser(id);
 
         //then
         assertThat(result.getAddresses()).containsExactlyInAnyOrderElementsOf(user.getAddresses());
@@ -62,7 +100,7 @@ public class DatabaseServiceTest {
         when(userRepository.getUser(eq(id))).thenReturn(Optional.empty());
 
         //when
-        userDbService.getUser(id);
+        databaseService.getUser(id);
 
         //then
 
